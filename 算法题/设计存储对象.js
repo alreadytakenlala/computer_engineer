ec743b015dadc911fad33016c98028515b23f5ea
alreadytakenlala <1285380545@qq.com>
2020-05-29 11:32:17 +0800

    first commit

---
+++ 算法题/设计存储对象.js
@@ -0,0 +1,48 @@
+class LRUCache {
+	constructor(leg) {
+	    this.data = {};
+		this.arr = [];
+		this.leg = leg;
+	}
+	put(key, value) {
+		this.arr.push(key);
+		if (this.arr.length > this.leg) {
+			delete this.data[this.arr[0]];
+			this.arr.shift();
+		}
+		this.data[key] = {
+			value,
+			index: this.arr.length-1
+		};
+	}
+	get(key) {
+		let item = this.data[key];
+		if (!item) return -1;
+		let temp = this.arr[this.arr.length-1];
+		this.arr[this.arr.length-1] = this.arr[item.index];
+		this.arr[item.index] = temp;
+		return item.value;
+	}
+}
+
+const cache = new LRUCache(2);
+
+cache.put(1,1);
+
+cache.put(2,2);
+
+console.log(cache.get(1)); //返回1
+
+cache.put(3,3); //去除key 2
+
+console.log(cache.get(2)); //返回-1(未找到key 2)
+
+console.log(cache.get(3));  //返回3
+
+cache.put(4,4); //去除key 1
+
+console.log(cache.get(1)); //返回-1
+
+console.log(cache.get(3));  //返回3
+
+console.log(cache.get(4));  //返回4
